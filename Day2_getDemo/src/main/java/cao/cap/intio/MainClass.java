e55c073792b8a6a41c00582b1d61e655b48f75cb
lnc <lnc@DIN76000835.corp.capgemini.com>
2017-11-29 10:14:21 +0530

    commit all

---
+++ Day2_getDemo/src/main/java/cao/cap/intio/MainClass.java
@@ -0,0 +1,17 @@
+package cao.cap.intio;
+
+public class MainClass {
+
+	Orderclass o=new Orderclass();
+	
+	public static void m1(){
+		System.out.println("nsdgf");
+	}
+	public static void main(String[] args) {
+		Orderclass o=new Orderclass();
+		o.m2();
+		m1();
+
+	}
+
+}
---
+++ Day2_getDemo/src/main/java/cao/cap/intio/Orderclass.java
@@ -0,0 +1,10 @@
+package cao.cap.intio;
+
+public class Orderclass {
+
+	
+	public void m2(){
+		
+		System.out.println(":prasaf");
+	}
+}
